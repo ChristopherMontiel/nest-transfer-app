b2401afd7fb62258cf9011055dc40f09626dce1d
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-24 11:43:40 -0400

    [ADD] Se añade endpoint para documentación con módulo Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Transfer-APP API documentation')
+  .setDescription('API para mi sitio de transferencias')
+  .setVersion('1.0')
+  .addTag('destinatarios')
+  .addTag('transferencias')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

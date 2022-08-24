cfa9eaf79144f95d27f488e2348407718c5a1795
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-24 11:23:35 -0400

    [ADD] Se añade resource con CRUD base de transferencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DestinatariosModule } from './destinatarios/destinatarios.module';
+import { TransferenciasModule } from './transferencias/transferencias.module';
 
 @Module({
   //imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
-  imports: [MongooseModule.forRoot('mongodb://mongo:w5b8Ta2Y5KtM6W0iEA0o@containers-us-west-18.railway.app:6613'), DestinatariosModule],
+  imports: [MongooseModule.forRoot('mongodb://mongo:w5b8Ta2Y5KtM6W0iEA0o@containers-us-west-18.railway.app:6613'), DestinatariosModule, TransferenciasModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/transferencias/transferencias.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TransferenciasService } from './transferencias.service';
+import { TransferenciasController } from './transferencias.controller';
+
+@Module({
+  controllers: [TransferenciasController],
+  providers: [TransferenciasService]
+})
+export class TransferenciasModule {}

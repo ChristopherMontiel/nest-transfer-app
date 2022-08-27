b44f55ec67e5e15b8ac91e7457fe2a2088427d71
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-27 14:06:02 -0400

    [ADD] Se añaden comentarios

--- src/recipients/dto/create-recipient.dto.ts
+++ src/recipients/dto/create-recipient.dto.ts
@@ -1,34 +1,42 @@
 import { IsAlphanumeric, IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator";
 
 export class CreateRecipientDto {
+  //Nombre Destinatario
   @IsNotEmpty()
   @IsString()
   name: string;
   
+  //Número de Rut Destinatario
   @IsNotEmpty()
   @IsNumber()
   rutNumber: number;
 
+  //DV Destinatario
   @IsAlphanumeric()
   @IsNotEmpty()
   rutDv: string;
 
+  //mail Destinatario
   @IsNotEmpty()
   @IsEmail()
   mail: string;
 
+  //N° de teléfono Destinatario
   @IsNotEmpty()
   @IsPhoneNumber()
   phone: string;
 
+  //Banco Destinatario
   @IsNotEmpty()
   @IsString()
   bank: string;
 
+  //Tipo de cuenta
   @IsNotEmpty()
   @IsString()
   accType: string;
 
+  //N° de cuenta bancaria
   @IsNotEmpty()
   @IsNumber()
   accNumber: number;

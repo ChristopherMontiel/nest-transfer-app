73377b1a7693e5dea219db2c1265fe65e562ede2
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-27 14:06:34 -0400

    [ADD] Se añaden validaciones para transfer en DTO

--- src/transfers/dto/create-transfer.dto.ts
+++ src/transfers/dto/create-transfer.dto.ts
@@ -1,1 +1,48 @@
-export class CreateTransferDto {}
+import { IsAlphanumeric, IsDateString, IsMilitaryTime, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateTransferDto {
+  //Nombre destinatario
+  @IsNotEmpty()
+  @IsString()
+  recName: string;
+
+  //Número de Rut destinatario
+  @IsNotEmpty()
+  @IsNumber()
+  recRutNumber: number;
+
+  //DV destinatario
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  recDv: string;
+
+  //Banco destino
+  @IsNotEmpty()
+  @IsString()
+  recBank: string;
+
+  //Tipo de cuenta destino
+  @IsNotEmpty()
+  @IsString()
+  recAccType: string;
+
+  //Monto transferido
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+  
+  //Fecha de la transferencia
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+
+  //Hora de la transferencia
+  @IsNotEmpty()
+  @IsMilitaryTime()
+  time: string;
+
+  //N° Cuenta de origen (No necesario por ahora.)
+  /*@IsNotEmpty()
+  @IsNumber()
+  originAccount: number;*/
+}

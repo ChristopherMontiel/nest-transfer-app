1888a776f4517d8c59e66c4ef2783d1fd406122a
Christopher Montiel <christopher.montiel.m@gmail.com>
2022-08-27 13:17:57 -0400

    [ADD] Se añade import de esquema de transfers

--- src/transfers/transfers.module.ts
+++ src/transfers/transfers.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TransfersService } from './transfers.service';
 import { TransfersController } from './transfers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { transfers, transfersSchema } from './schema/transfers.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: transfers.name,
+        schema: transfersSchema
+      }
+    ])
+  ],
   controllers: [TransfersController],
   providers: [TransfersService]
 })
